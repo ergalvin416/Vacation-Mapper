0207351b561a6407db2959bab47cee60bb1bf35b
ergalvin416 <139143918+ergalvin416@users.noreply.github.com>
2026-01-20 10:31:47 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAlflCNqAaVmui0ipNCCWs3XNsvjprlFYA",
+    authDomain: "vacation-mapper-1e723.firebaseapp.com",
+    databaseURL: "https://vacation-mapper-1e723-default-rtdb.firebaseio.com",
+    projectId: "vacation-mapper-1e723",
+    storageBucket: "vacation-mapper-1e723.firebasestorage.app",
+    messagingSenderId: "773407322730",
+    appId: "1:773407322730:web:9b27eb7ee5c3ebac2cd013"
+};
+
+const GOOGLE_MAPS_KEY = "AIzaSyC78EKBgSd2UsLSPUTGO1A-MF78BU20aLg";
